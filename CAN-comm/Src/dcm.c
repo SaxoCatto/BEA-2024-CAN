4b2d5b75b68d20bfca6e87b2f6029d27adc1b622
Saxo Cat <92432250+SaxoCatto@users.noreply.github.com>
2024-08-26 22:43:01 +0700

    Delete CAN-comm/Src directory

--- CAN-comm/Src/dcm.c
+++
@@ -1,9 +0,0 @@
-/*********************************************************/
-/*********BOSCH BEA PROGRAM SKELETON DEMO CODE************/
-/*********************************************************/
-
-#include "dcm.h"
-/*for further services please add service header here*/
-#include "dcm_rdbi.h"
-#include "dcm_wdbi.h"
-#include "dcm_seca.h"
